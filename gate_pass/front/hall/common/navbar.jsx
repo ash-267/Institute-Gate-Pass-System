f27d9c8e07aebc787ed64588cb023a571b6d7486
ash-267 <ayushsadavarte2024.comp@mmcoe.edu.in>
2026-03-12 14:21:00 +0530

    Refactor Navbar component layout and links
    Refactor Navbar component to use semantic HTML and improve structure.

--- gate_pass/front/hall/common/navbar.jsx
+++ gate_pass/front/hall/common/navbar.jsx
@@ -1,19 +1,20 @@
 import '../common/navbar.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-const Navbar = ()=>{
-    return <>
-    <div className="navbar">
-        <Link to={"/App"}>
-            <h2>Home</h2>
-        </Link>
-            
-        
-        <Link to={"/search"}>
-            Search
-        </Link>
-    </div>
-    </>
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="nav-left">
+                <Link to="/" className="nav-home">HOME</Link>
+            </div>
+
+            <div className="nav-right">
+                <Link to="/login" className="nav-link">Login</Link>
+                {/* MMCOE Logo */}
+                <img src="https://mmcoe.edu.in/wp-content/uploads/2024/09/mmcoe_newlogo-3.png" alt="MMCOE Logo" className="nav-logo" />
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar;
