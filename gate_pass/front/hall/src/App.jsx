5eaa481e94f29ce8f44a951ca00c9d44d362f565
ash-267 <ayushsadavarte2024.comp@mmcoe.edu.in>
2026-03-12 14:11:45 +0530

    Add routing and new pages to App component

--- gate_pass/front/hall/src/App.jsx
+++ gate_pass/front/hall/src/App.jsx
@@ -1,17 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from '../common/navbar'
 import Actual from '../1stUI/actual'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import SecurityDashboard from './pages/SecurityDashboard'
+import HostApproval from './pages/HostApproval'
+import AdminPanel from './pages/AdminPanel'
+import Signup from './pages/Signup'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Navbar></Navbar>
-      <Actual></Actual>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Actual />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/security" element={<SecurityDashboard />} />
+        <Route path="/host" element={<HostApproval />} />
+        <Route path="/admin" element={<AdminPanel />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </>
   )
 }
