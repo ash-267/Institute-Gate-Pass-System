bfeecc693fe5c991cb8b64e15805ecfb4effb787
ash-267 <ayushsadavarte2024.comp@mmcoe.edu.in>
2026-03-13 08:59:27 +0530

    Add department data with names and floors

---
+++ gate_pass/front/hall/src/departmentData.js
@@ -0,0 +1,13 @@
+const DEPARTMENTS = [
+    { name: "Artificial Intelligence and Data Science", floor: "6th Floor" },
+    { name: "Engineering Sciences and Humanities", floor: "6th Floor" },
+    { name: "Computer Engineering", floor: "5th Floor" },
+    { name: "Information Technology", floor: "4th Floor" },
+    { name: "Electronics and Telecommunication", floor: "3rd Floor" },
+    { name: "Mechanical Engineering", floor: "2nd Floor" },
+    { name: "Electrical Engineering", floor: "1st Floor" },
+    { name: "FMCIII", floor: "Ground Floor" },
+    { name: "Student Section", floor: "Ground Floor" },
+];
+
+export default DEPARTMENTS;
